eac1fe20527b1d809bc4949ac06a4e7dc26b14f8
Brigham Mueller <brigmu@gmail.com>
2020-02-26 19:37:09 -0800

    added prompts to get user input for type of employee, employee information, team name, and continue to get input if they want to add more

--- app.js
+++ app.js
@@ -1,35 +1,181 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const render = require('./lib/htmlRenderer');
+
+let questions = [
+    {
+        message: 'Enter employee name',
+        name: 'name'
+    },
+    {
+        message: 'Enter employee ID',
+        name: 'id'
+    },
+    {
+        message: 'Enter employee email',
+        name: 'email'
+    }
+];
+
+const managerQuestion = {
+    message: 'Enter office number',
+    name: 'officeNumber'
+}
+
+const engineerQuestion = {
+    message: 'Enter employee github username',
+    name: 'github'
+}
+
+const internQuestion = {
+    message: 'Enter employee school',
+    name: 'school'
+}
+
+const employees = [];
+
+const init = async function () {
+    try {
+        const { begin } = await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Create a new team',
+                name: 'begin',
+                choices: ['Yes', 'No']
+            }
+        ]);
+
+        if (begin === 'Yes') {
+
+            promptData();
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const promptData = async function() {
+    try {    
+        const {employeeType} = await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Choose type of employee',
+                name: 'employeeType',
+                choices: ['Manager', 'Engineer', 'Intern']
+            }
+        ])
+    
+        // console.log(employeeType);
+    
+        if(employeeType === 'Manager'){
+            questions.push(managerQuestion);
+            const {name, id, email, officeNumber} = await inquirer.prompt(questions);
+            const a = new Manager(name, id, email, officeNumber);
+            questions = questions.splice(0, 3);
+    
+            employees.push(a);
+            // console.log(employees);
+    
+            const {addPrompt} = await inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Add another employee?',
+                    name: 'addPrompt',
+                    choices: ['Yes', 'No']
+                }
+            ]);
+    
+            if(addPrompt === 'Yes') {
+                promptData();
+            }
+            // const test1 = a.getRole();
+            // console.log(test1);
+            // const test2 = a.getName();
+            // console.log(test2);
+            // const test3 = a.getId();
+            // console.log(test3);
+            // const test4 = a.getOfficeNumber();
+            // console.log(test4);
+        }
+        else if (employeeType === 'Engineer') {
+            questions.push(engineerQuestion);
+            const {name, id, email, github} = await inquirer.prompt(questions);
+            const b = new Engineer(name, id, email, github);
+            questions = questions.splice(0, 3);
+    
+            employees.push(b);
+            // console.log(employees);
+    
+            const {addPrompt} = await inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Add another employee?',
+                    name: 'addPrompt',
+                    choices: ['Yes', 'No']
+                }
+            ]);
+    
+            if(addPrompt === 'Yes') {
+                promptData();
+            }
+    
+        }
+        else if (employeeType === 'Intern') {
+            try {
+                questions.push(internQuestion);
+                const { name, id, email, school } = await inquirer.prompt(questions);
+                const c = new Intern(name, id, email, school);
+                questions = questions.splice(0, 3);
+    
+                employees.push(c);
+                // console.log(employees);
+    
+                const { addPrompt } = await inquirer.prompt([
+                    {
+                        type: 'list',
+                        message: 'Add another employee?',
+                        name: 'addPrompt',
+                        choices: ['Yes', 'No']
+                    }
+                ]);
+    
+                if (addPrompt === 'Yes') {
+                    promptData();
+                } else {
+                    const html = render(employees);
+    
+                    const { teamName } = await inquirer.prompt([
+                        {
+                            message: 'Enter team name',
+                            name: 'teamName'
+                        }
+                    ]);
+    
+                    // fs.writeFileSync(outputPath, html, 'utf-8', () =>{
+                    //     console.log('Wrote file');
+                    // })
+                }
+    
+            }
+        
+            catch (error) {
+                console.log(error);
+            }
+        }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    
+    
+    init();
